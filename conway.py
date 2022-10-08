336dd4795073cb272d1da322c5a689cc479e8386
Poteresting <63595223+poteresting@users.noreply.github.com>
2022-10-09 02:02:22 +0300

    Update conway.py

--- conway.py
+++ conway.py
@@ -6,9 +6,9 @@ for y in range(20):
     my_list = []
     for x in range(80):
         if random.randint(0, 1) == 0:
-            my_list.append('O')
+            my_list.append('1')
         else:
-            my_list.append('.')
+            my_list.append('0')
     nextcell.append(my_list)
 
 while True:
@@ -27,27 +27,27 @@ while True:
             right = (x+1) % 80
             up = (y-1) % 20
             down = (y+1) % 20
-            if current[y][left] == 'O':
+            if current[y][left] == '1':
                 neighbor += 1
-            if current[y][right] == 'O':
+            if current[y][right] == '1':
                 neighbor += 1
-            if current[up][x] == 'O':
+            if current[up][x] == '1':
                 neighbor += 1
-            if current[down][x] == 'O':
+            if current[down][x] == '1':
                 neighbor += 1
-            if current[up][left] == 'O':
+            if current[up][left] == '1':
                 neighbor += 1
-            if current[down][left] == 'O':
+            if current[down][left] == '1':
                 neighbor += 1
-            if current[up][right] == 'O':
+            if current[up][right] == '1':
                 neighbor += 1
-            if current[down][right] == 'O':
+            if current[down][right] == '1':
                 neighbor += 1
 
-            if current[y][x] == 'O' and (neighbor == 2 or neighbor == 3):
-                nextcell[y][x] = 'O'
-            elif current[y][x] == '.' and neighbor == 3:
-                nextcell[y][x] = 'O'
+            if current[y][x] == '1' and (neighbor == 2 or neighbor == 3):
+                nextcell[y][x] = '1'
+            elif current[y][x] == '0' and neighbor == 3:
+                nextcell[y][x] = '1'
             else:
-                nextcell[y][x] = '.'
+                nextcell[y][x] = '0'
     time.sleep(1)
