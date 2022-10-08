b0715980f5ac4f8a2625416bf43817e3a600ff95
Poteresting <63595223+poteresting@users.noreply.github.com>
2022-10-09 02:52:22 +0300

    Update conway.py

--- conway.py
+++ conway.py
@@ -2,52 +2,52 @@ import random, copy, time
 
 nextcell = []
 
-for y in range(20):
+for y in range(23):
     my_list = []
     for x in range(80):
         if random.randint(0, 1) == 0:
-            my_list.append('1')
+            my_list.append('o')
         else:
-            my_list.append('0')
+            my_list.append(' ')
     nextcell.append(my_list)
 
 while True:
     print('\n\n\n\n\n\n')
     
-    for y in range(20):
+    for y in range(23):
         for x in range(80):
             print(nextcell[y][x], end='')
         print()
     current = copy.deepcopy(nextcell)
 
-    for y in range(20):
+    for y in range(23):
         for x in range (80):
             neighbor = 0
             left = (x-1) % 80
             right = (x+1) % 80
             up = (y-1) % 20
             down = (y+1) % 20
-            if current[y][left] == '1':
+            if current[y][left] == 'o':
                 neighbor += 1
-            if current[y][right] == '1':
+            if current[y][right] == 'o':
                 neighbor += 1
-            if current[up][x] == '1':
+            if current[up][x] == 'o':
                 neighbor += 1
-            if current[down][x] == '1':
+            if current[down][x] == 'o':
                 neighbor += 1
-            if current[up][left] == '1':
+            if current[up][left] == 'o':
                 neighbor += 1
-            if current[down][left] == '1':
+            if current[down][left] == 'o':
                 neighbor += 1
-            if current[up][right] == '1':
+            if current[up][right] == 'o':
                 neighbor += 1
-            if current[down][right] == '1':
+            if current[down][right] == 'o':
                 neighbor += 1
 
-            if current[y][x] == '1' and (neighbor == 2 or neighbor == 3):
-                nextcell[y][x] = '1'
-            elif current[y][x] == '0' and neighbor == 3:
-                nextcell[y][x] = '1'
+            if current[y][x] == 'o' and (neighbor == 2 or neighbor == 3):
+                nextcell[y][x] = 'o'
+            elif current[y][x] == ' ' and neighbor == 3:
+                nextcell[y][x] = 'o'
             else:
-                nextcell[y][x] = '0'
-    time.sleep(1)
+                nextcell[y][x] = ' '
+    time.sleep(0.2)
